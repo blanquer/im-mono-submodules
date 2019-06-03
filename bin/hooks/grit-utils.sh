f88f2ab0048dfa94ec678e1ebd548ed6aa6fdc1b
Josep M. Blanquer <blanquer@gmail.com>
2019-06-03 15:10:04 -0700

    took reporting functions into common utils file

---
+++ bin/hooks/grit-utils.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Utilities
+
+GRIT_PREFIX="Grit:"
+info() {
+  local blue='\033[0;34m'
+  GRIT="${blue}${GRIT_PREFIX}\033[0m"
+  printf "$GRIT $1\n"
+}
+warn() {
+  local red='\033[0;31m'
+  GRIT="${red}${GRIT_PREFIX}\033[0m"
+  printf "$GRIT $1\n"
+}
+
+err() {
+  local red='\033[0;32m'
+  GRIT="${red}${GRIT_PREFIX}\033[0m"
+  printf "$GRIT $1\n"
+}
